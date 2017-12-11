affa8f4b03b8c0b11bf0e4ed010a2d13174a02a6
root <root@localhost.localdomain>
2017-12-11 16:32:02 +0000

    Add a Main.cpp
    At this point, we got our first window !!!

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,38 @@
 
 #include <QApplication>
 #include <QtGui>
-#include "CellViewer.h"
+#include <string>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <memory>
+using namespace std;
 
+#include "CellViewer.h"
+#include "Term.h"
+#include "Net.h"
+#include "Instance.h"
+#include "Cell.h"
 using namespace Netlist;
 
-int   main (int argc, char* argv[]) {
-  QApplication*   qa = new QApplication(argc, argv);
 
-  /* CellViewer* viewer = new CellViewer();
+int main ( int argc, char* argv[] )
+{
+  QApplication* qa = new QApplication(argc, argv);
+
+  cout << "Chargement des modeles..." << endl;
+  Cell::load( "vdd" );
+  Cell::load( "gnd" );
+  Cell::load( "TransistorN" );
+  Cell::load( "TransistorP" );
+  Cell::load( "and2" );
+  Cell::load( "or2" );
+  Cell* xor2      = Cell::load( "xor2" );
+  Cell* halfadder = Cell::load( "halfadder" );
+
+  CellViewer* viewer = new CellViewer();
   viewer->setCell(halfadder);
-  viewer->show(); */
+  viewer->show();
 
   int rvalue = qa->exec();
   delete qa;
